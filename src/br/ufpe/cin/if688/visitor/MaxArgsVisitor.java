09b2c3273bec4a1faf857ecae03428829f39812f
afc2 <afc2@cin.ufpe.br>
2017-09-21 11:43:02 -0300

    atividade 3 - interpretando ASTs

---
+++ src/br/ufpe/cin/if688/visitor/MaxArgsVisitor.java
@@ -0,0 +1,91 @@
+package br.ufpe.cin.if688.visitor;
+
+import br.ufpe.cin.if688.ast.AssignStm;
+import br.ufpe.cin.if688.ast.CompoundStm;
+import br.ufpe.cin.if688.ast.EseqExp;
+import br.ufpe.cin.if688.ast.Exp;
+import br.ufpe.cin.if688.ast.ExpList;
+import br.ufpe.cin.if688.ast.IdExp;
+import br.ufpe.cin.if688.ast.LastExpList;
+import br.ufpe.cin.if688.ast.NumExp;
+import br.ufpe.cin.if688.ast.OpExp;
+import br.ufpe.cin.if688.ast.PairExpList;
+import br.ufpe.cin.if688.ast.PrintStm;
+import br.ufpe.cin.if688.ast.Stm;
+
+public class MaxArgsVisitor implements IVisitor<Integer> {
+
+	@Override
+	public Integer visit(Stm s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer visit(AssignStm s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer visit(CompoundStm s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer visit(PrintStm s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer visit(Exp e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer visit(EseqExp e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer visit(IdExp e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer visit(NumExp e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer visit(OpExp e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer visit(ExpList el) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer visit(PairExpList el) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer visit(LastExpList el) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
